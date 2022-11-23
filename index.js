b0b7cf5ffc6192a1e976c7f41a36a0c62b58a977
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-23 21:16:04 +0800

    setup

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+const MongoUtil = require("./MongoUtil");
+
+//set up express app
+const app = express();
+//enable json processing
+app.use(express.json());
+//enable cors
+app.use(cors());
+
+async function main(){
+    await MongoUtil.connect(process.env.MONGO_URI, "tgc20-test");
+    const db = MongoUtil.getDB();
+
+    // app.get("/", function (req, res){
+    //     res.send("helloworld")
+    // })
+
+    app.get("/brands", async function(req,res){
+        let response = await db.collection("brands").find().toArray();
+        res.json(response);
+    })
+
+    app.post("/create-brand", async function (req,res){
+        let brandName = req.body.brandName;
+        
+        try{
+            let response = await db.collection("brands").insertOne({
+                brandName
+            })
+            res.status(200);
+            res.json(response);
+        }catch(e){
+            res.status(500);
+            res.json({
+                "message": "Internal server error. Please contact administrator"
+            })
+            console.log(e)
+        }
+    })
+
+    app.get("/countries", async function(req,res){
+        let response = await db.collection("countries").find().toArray();
+        res.json(response);
+    })
+
+    app.post("/create-country", async function(req,res){
+        let country = req.body.country;
+
+        try{
+            let response = await db.collection("countries").insertOne({
+                country
+            })
+            res.status(200);
+            res.json(response);
+        }catch(e){
+            res.status(500);
+            res.json({
+                "message": "Internal server error. Please contact administrator"
+            })
+            console.log(e)
+        }
+    })
+
+    app.get("/listings", async function(req,res){
+        let response = await db.collection("listings").find().toArray();
+        res.json(response);
+    })
+
+    app.post("/create-listing", async function(req,res){
+        let name = req.body.name;
+        let country = ObjectId(req.body.countryId);
+        let brand = ObjectId(req.body.brandId);
+
+        try{
+            let response = await db.collection("listings").insertOne({
+                name,
+                country,
+                brand
+            })
+            res.status(200);
+            res.json(response);
+        } catch(e){
+            res.status(500);
+            res.json({
+                "message": "Internal server error. Please contact administrator"
+            })
+            console.log(e)
+        }
+    })
+
+}
+main();
+
+//open the listening port
+app.listen(process.env.PORT, function () {
+    console.log("server started")
+})
