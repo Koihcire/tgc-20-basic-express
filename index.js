941c2071c3d4509b01a11d0a1652b9bbf2171a86
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-11-24 19:21:40 +0800

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //enable cors
 app.use(cors());
 
-async function main(){
+async function main() {
     await MongoUtil.connect(process.env.MONGO_URI, "tgc20-test");
     const db = MongoUtil.getDB();
 
@@ -19,21 +19,21 @@ async function main(){
     //     res.send("helloworld")
     // })
 
-    app.get("/brands", async function(req,res){
+    app.get("/brands", async function (req, res) {
         let response = await db.collection("brands").find().toArray();
         res.json(response);
     })
 
-    app.post("/create-brand", async function (req,res){
+    app.post("/create-brand", async function (req, res) {
         let brandName = req.body.brandName;
-        
-        try{
+
+        try {
             let response = await db.collection("brands").insertOne({
                 brandName
             })
             res.status(200);
             res.json(response);
-        }catch(e){
+        } catch (e) {
             res.status(500);
             res.json({
                 "message": "Internal server error. Please contact administrator"
@@ -42,21 +42,90 @@ async function main(){
         }
     })
 
-    app.get("/countries", async function(req,res){
+    app.get("/countries", async function (req, res) {
         let response = await db.collection("countries").find().toArray();
         res.json(response);
     })
 
-    app.post("/create-country", async function(req,res){
+    app.post("/create-country", async function (req, res) {
         let country = req.body.country;
 
-        try{
+        try {
             let response = await db.collection("countries").insertOne({
                 country
             })
             res.status(200);
             res.json(response);
-        }catch(e){
+        } catch (e) {
+            res.status(500);
+            res.json({
+                "message": "Internal server error. Please contact administrator"
+            })
+            console.log(e)
+        }
+    })
+   
+    // JOIN USING AGGREGATE
+    // app.get("/listings", async function(req,res){
+    //     let response = await db.collection("listings").aggregate([
+    //         {
+    //             $lookup: {
+    //                 from: "brands",
+    //                 localField: "brandId",
+    //                 foreignField: "_id",
+    //                 as: "brand",
+    //             }
+    //         },
+    //         {
+    //             $lookup: {
+    //                 from: "countries",
+    //                 localField: "countryId",
+    //                 foreignField: "_id",
+    //                 as: "country",
+    //             }
+    //         }
+    //     ]).toArray();
+    //     res.json(response);
+    // })
+
+    // JOIN USING MANUAL JOIN
+    app.get("/listings", async function (req, res) {
+        try {
+            let listingsResponse = await db.collection("listings").find().toArray();
+            let brandsResponse = await db.collection("brands").find().toArray();
+            let countriesResponse = await db.collection("countries").find().toArray();
+
+            let listingsData = listingsResponse;
+            // console.log(listingsData);
+            let brandsData = brandsResponse;
+            // console.log(brandsData);
+            let countriesData = countriesResponse;
+            // console.log(countriesData);
+
+            for (let listing of listingsData) {
+                let listingBrandId = (listing.brandId).toString();
+                let listingCountryId = (listing.countryId).toString();
+
+                // console.log(listingId);
+                for (let brand of brandsData) {
+                    let brandId = (brand._id).toString();
+                    if (listingBrandId === brandId) {
+                        listing.brand = brand.brandName;
+                        break;
+                    }
+                }
+                for (let country of countriesData) {
+                    let countryId = (country._id).toString();
+                    if (listingCountryId === countryId) {
+                        listing.country = country.country;
+                        break;
+                    }
+                }
+            }
+
+            res.status(200);
+            res.json(listingsData);
+        } catch (e) {
             res.status(500);
             res.json({
                 "message": "Internal server error. Please contact administrator"
@@ -65,41 +134,12 @@ async function main(){
         }
     })
 
-    //JOIN AT REACT METHOD (NEED TO MANUALLY JOIN COLLECTIONS AT REACT SIDE)
-    // app.get("/listings", async function(req,res){
-    //     let response = await db.collection("listings").find().toArray();
-    //     res.json(response);
-    // })
-
-    // JOIN AT EXPRESS METHOD
-    app.get("/listings", async function(req,res){
-        let response = await db.collection("listings").aggregate([
-            {
-                $lookup: {
-                    from: "brands",
-                    localField: "brandId",
-                    foreignField: "_id",
-                    as: "brand",
-                }
-            },
-            {
-                $lookup: {
-                    from: "countries",
-                    localField: "countryId",
-                    foreignField: "_id",
-                    as: "country",
-                }
-            }
-        ]).toArray();
-        res.json(response);
-    })
-
-    app.post("/create-listing", async function(req,res){
+    app.post("/create-listing", async function (req, res) {
         let name = req.body.name;
         let countryId = ObjectId(req.body.countryId);
         let brandId = ObjectId(req.body.brandId);
 
-        try{
+        try {
             let response = await db.collection("listings").insertOne({
                 name,
                 countryId,
@@ -107,7 +147,7 @@ async function main(){
             })
             res.status(200);
             res.json(response);
-        } catch(e){
+        } catch (e) {
             res.status(500);
             res.json({
                 "message": "Internal server error. Please contact administrator"
